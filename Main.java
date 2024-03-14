352f031617ec257eb7bc400d0eb8746d228f0f80
KotForLove <m.kotkovskyi@ukma.edu.ua>
2024-03-15 01:36:47 +0200

    Відсортувати знайдені результати алгоритмом merge sort  по кількості входженнь (asc), та вивести в консоль (stdout) "<кількість входжень> <індекс рядка у текстовому файлі починаючи з 0>".

--- Main.java
+++ Main.java
@@ -3,8 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        int a = 10;
+        int a = 10; //кількість рядків для тесту
         String[] lines = new String[a]; // Масив для збереження рядків
         int count = 0; // Лічильник для кількості зчитаних рядків
 
@@ -17,7 +16,9 @@ public class Main {
         Scanner inputScanner = new Scanner(System.in);
         String substringToSearch = inputScanner.nextLine();
 
-        // Функція для знаходження всіх входжень підрядка у кожному з рядків
+        int[] occurrencesArray = new int[count]; // Масив для збереження кількості входжень в кожному рядку
+
+        // Знайти кількість входжень підрядка в кожному рядку і зберегти їх у масив
         for (int i = 0; i < count; i++) {
             int lastIndex = 0;
             int occurrences = 0;
@@ -28,9 +29,68 @@ public class Main {
                     lastIndex += substringToSearch.length();
                 }
             }
-            System.out.println("Occurrences of \"" + substringToSearch + "\" in line " + i + ": " + occurrences);
+            occurrencesArray[i] = occurrences;
         }
 
+        // Сортування масиву occurrencesArray за допомогою алгоритму сортування злиттям
+        mergeSort(occurrencesArray, 0, count - 1);
 
+        // Вивести результати у форматі "<кількість входжень> <індекс рядка у текстовому файлі починаючи з 0>"
+        for (int i = 0; i < count; i++) {
+            System.out.println(occurrencesArray[i] + " " + lines[i]);
+        }
+
+    }
+
+    // Функція сортування злиттям
+    public static void mergeSort(int[] array, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(array, left, mid);
+            mergeSort(array, mid + 1, right);
+            merge(array, left, mid, right);
+        }
+    }
+
+    // Функція поєднання двох підмасивів
+    public static void merge(int[] array, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            L[i] = array[left + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            R[j] = array[mid + 1 + j];
+        }
+
+        int i = 0, j = 0;
+        int k = left;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                array[k] = L[i];
+                i++;
+            } else {
+                array[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            array[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            array[k] = R[j];
+            j++;
+            k++;
+        }
     }
 }
